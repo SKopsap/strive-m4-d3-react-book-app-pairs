45d8677177a53bfc9de9033062641a0027c30f22
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2021-03-25 00:57:01 +0100

    style added

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,15 +1,18 @@
+import { Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
 function BookList({ fantasy }) {
   return (
     <div>
-      {fantasy.map((book) => {
-        return (
-          <div key={book.asin}>
-            <SingleBook name={book.asin} title={book.title} img={book.img} />
-          </div>
-        );
-      })}
+      <Row>
+        {fantasy.map((book) => {
+          return (
+            <div key={book.asin}>
+              <SingleBook name={book.asin} title={book.title} img={book.img} />
+            </div>
+          );
+        })}
+      </Row>
     </div>
   );
 }
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
 const SingleBook = ({ title, img }) => {
   const [shit, updatedShit] = useState(false);
 
   const toggleClass = () => updatedShit(!shit);
   return (
-    <Card
-      onClick={toggleClass}
-      className={`mt-3 ${shit && "selected"}`}
-      style={{ width: "250px" }}
-    >
-      <Card.Img src={img} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-      </Card.Body>
-    </Card>
+    <Col xs={12} sm={6} md={4} lg={3} className="book">
+      <Card
+        onClick={toggleClass}
+        className={`mt-3 ${shit && "selected"}`}
+        style={{ width: "18rem" }}
+      >
+        <Card.Img lazyLoad={true} src={img} height="350" />
+        <Card.Body
+          style={{
+            height: "170px",
+          }}
+        >
+          <Card.Title
+            style={{
+              textOverflow: "ellipsis",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+            }}
+          >
+            {title}
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
