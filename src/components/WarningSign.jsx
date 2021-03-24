84fa4329b4cf0fa8f6dc71ec44959c017101a1a9
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2021-03-24 15:34:54 +0100

    fixes #1, fixes #2, fixes #3 converted functional component to class :(

--- src/components/WarningSign.jsx
+++ src/components/WarningSign.jsx
@@ -1,16 +1,30 @@
 import { Alert, Button } from "react-bootstrap";
-import { useState } from "react";
-function WarningSign(props) {
-  const [show, setShow] = useState(true);
+import React from "react";
+class WarningSign extends React.Component {
+  state = {
+    show: false,
+  };
 
-  if (show) {
+  if(show) {
     return (
-      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>{props.text}</Alert.Heading>
-        <p>{props.text}</p>
+      <Alert
+        variant="danger"
+        onClose={() => this.setState({ show: false })}
+        dismissible
+      >
+        <Alert.Heading>{this.props.text}</Alert.Heading>
+        <p>{this.props.text}</p>
       </Alert>
     );
   }
-  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+  render() {
+    return (
+      <>
+        <Button onClick={() => this.setState({ show: true })}>
+          Show Alert
+        </Button>
+      </>
+    );
+  }
 }
 export default WarningSign;
